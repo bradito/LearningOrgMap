c3bd880f166480b20436e0ac8b85a1e377a0e5a6
Bradley Gordon <bradito@mac.com>
2017-03-02 07:55:09 -0600

    my first package!

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='LearningOrgMap',
+    packages=['LearningOrgMap'],
+    include_package_data=True,
+    install_requires=[
+        'flask',
+    ],
+)
