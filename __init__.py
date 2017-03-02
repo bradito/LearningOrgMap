e6398e9c5787b1db0d89aa479e198bc1fbc4bf62
Bradley Gordon <bradito@mac.com>
2017-03-02 07:39:06 -0600

    config file add
    add server port and debug status to a config file

--- __init__.py
+++ __init__.py
@@ -11,6 +11,19 @@
 
 from flask import Flask, render_template, request, send_from_directory
 import datetime, csv
+import sys
+
+try: 
+	import serverconfig
+except: 
+	sys.exit(
+"""		
+
+Missing Config!
+You need to copy serverconfig_EXAMPLE.py to become serverconfig.py to run program with proper configuration
+				
+""")
+
 app = Flask(__name__,static_url_path='')
 
 @app.route("/")
@@ -44,4 +57,4 @@ def send_css(path):
     return send_from_directory('css', path)
 
 if __name__ == "__main__":
-	app.run(host='0.0.0.0', port=8080, debug=True)
+	app.run(host='0.0.0.0', port=serverconfig.server_port, debug=serverconfig.server_debug)
---
+++ serverconfig_EXAMPLE.py
@@ -0,0 +1,3 @@
+#!/usr/bin/env python
+server_port = 8080
+server_debug = True
