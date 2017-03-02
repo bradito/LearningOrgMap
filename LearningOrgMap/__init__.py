45539df909323b89f8855b26826c852942babdaa
Bradley Gordon <bradito@mac.com>
2017-03-02 08:48:18 -0600

    cleaning up config method again

--- LearningOrgMap/__init__.py
+++ LearningOrgMap/__init__.py
@@ -13,18 +13,13 @@ from flask import Flask, render_template, request, send_from_directory
 import datetime, csv
 import sys
 
-try: 
-	import serverconfig
-except: 
-	sys.exit(
-"""		
-
-Missing Config!
-You need to copy serverconfig_EXAMPLE.py to become serverconfig.py to run program with proper configuration
-				
-""")
 
 app = Flask(__name__,static_url_path='')
+
+try:
+	app.config.from_envvar('LEARNINGORGMAP_SETTINGS')
+except:
+	sys.exit("MISSING: settings file")
 
 @app.route("/")
 def hello():
@@ -57,4 +52,4 @@ def send_css(path):
     return send_from_directory('css', path)
 
 if __name__ == "__main__":
-	app.run(host='0.0.0.0', port=serverconfig.server_port, debug=serverconfig.server_debug)
+	app.run()
--- LearningOrgMap/serverconfig_EXAMPLE.py
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env python
-server_port = 8080
-server_debug = True
